2511f77409e954c93153f60cdd9bb4ffe8b08a0c
Matthew Little <zone117x@gmail.com>
2024-10-25 23:34:08 +0200

    feat: pg schema for mock payload tables

---
+++ migrations/1729684505758_mock_proposals.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.createTable('mock_proposals', {
+    id: {
+      type: 'bigserial',
+      primaryKey: true,
+    },
+    received_at: {
+      type: 'timestamptz',
+      notNull: true,
+    },
+    miner_key: {
+      type: 'bytea',
+      notNull: true,
+    },
+    burn_block_height: {
+      type: 'integer',
+      notNull: true,
+    },
+    stacks_tip_consensus_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+    // AKA block_hash
+    stacks_tip: {
+      type: 'bytea',
+      notNull: true,
+    },
+    // AKA block_height
+    stacks_tip_height: {
+      type: 'integer',
+      notNull: true,
+    },
+    server_version: {
+      type: 'text',
+      notNull: true,
+    },
+    pox_consensus_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+    network_id: {
+      type: 'integer',
+      notNull: true,
+    },
+    index_block_hash: {
+      type: 'bytea',
+      notNull: true,
+    },
+  });
+
+  pgm.createIndex('mock_proposals', ['received_at']);
+  pgm.createIndex('mock_proposals', ['stacks_tip_height']);
+  pgm.createIndex('mock_proposals', ['stacks_tip']);
+  pgm.createIndex('mock_proposals', ['index_block_hash']);
+  pgm.createIndex('mock_proposals', ['burn_block_height']);
+}
