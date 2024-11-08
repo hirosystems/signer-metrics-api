4f592e7e00907480b714dc6e72169fc0976c36e7
Matthew Little <zone117x@gmail.com>
2024-11-08 18:02:15 +0100

    fix: re-org issue with cycle-signer-set data (#53)

---
+++ tests/db/duplicate-signer-set-insert.test.ts
@@ -0,0 +1,44 @@
+import * as fs from 'node:fs';
+import { PgStore } from '../../src/pg/pg-store';
+import { RpcStackerSetResponse } from '../../src/stacks-core-rpc/stacks-core-rpc-client';
+import { rpcStackerSetToDbRewardSetSigners } from '../../src/stacks-core-rpc/stacker-set-updater';
+
+describe('Duplicate signer set insert', () => {
+  let db: PgStore;
+
+  beforeAll(async () => {
+    db = await PgStore.connect();
+  });
+
+  afterAll(async () => {
+    await db.close();
+  });
+
+  test('Insert initial signer set', async () => {
+    const stackerSetDump = JSON.parse(
+      fs.readFileSync('./tests/dumps/dump-stacker-set-cycle-72-2024-11-02.json', 'utf8')
+    ) as RpcStackerSetResponse;
+    const insertResult = await db.chainhook.insertRewardSetSigners(
+      db.sql,
+      rpcStackerSetToDbRewardSetSigners(stackerSetDump, 72)
+    );
+    expect(insertResult).toEqual({
+      rowsDeleted: 0,
+      rowsInserted: 11,
+    });
+  });
+
+  test('Overwrite signer set', async () => {
+    const stackerSetDump = JSON.parse(
+      fs.readFileSync('./tests/dumps/dump-stacker-set-cycle-72-2024-11-02.json', 'utf8')
+    ) as RpcStackerSetResponse;
+    const insertResult = await db.chainhook.insertRewardSetSigners(
+      db.sql,
+      rpcStackerSetToDbRewardSetSigners(stackerSetDump, 72)
+    );
+    expect(insertResult).toEqual({
+      rowsDeleted: 11,
+      rowsInserted: 11,
+    });
+  });
+});
