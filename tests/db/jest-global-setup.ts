b980f0264053c9cefb56d536d18f4be6749262ea
Matthew Little <zone117x@gmail.com>
2025-05-19 20:46:12 +0700

    test: spin up SNP server in integration tests

--- tests/db/jest-global-setup.ts
+++ tests/db/jest-global-setup.ts
@@ -1,5 +1,9 @@
+import * as net from 'node:net';
 import * as Docker from 'dockerode';
-import { connectPostgres } from '@hirosystems/api-toolkit';
+import { connectPostgres, timeout } from '@hirosystems/api-toolkit';
+import { createClient } from 'redis';
+
+const testContainerLabel = 'signer-metrics-pg-tests';
 
 const pgConfig = {
   PGHOST: '127.0.0.1',
@@ -35,62 +39,75 @@ async function pruneContainers(docker: Docker, label: string) {
     if (container.State !== 'exited') {
       await c.stop().catch(_err => {});
     }
-    await c.remove();
+    await c.remove({ v: true, force: true });
   }
+  await docker.pruneContainers({ filters: { label: [label] } });
   return containers.length;
 }
 
-async function startPostgresContainer(): Promise<void> {
-  const pgImage = 'postgres:17';
-  const label = 'signer-metrics-pg-tests';
+async function startContainer(args: {
+  docker: Docker;
+  image: string;
+  ports: { container: number; host: number }[];
+  env: string[];
+}) {
+  const { docker, image, ports, env } = args;
   try {
-    const docker = new Docker();
-    const imgPulled = await isDockerImagePulled(docker, pgImage);
+    const imgPulled = await isDockerImagePulled(docker, image);
     if (!imgPulled) {
-      console.log('Pulling PostgreSQL image...');
-      await pullDockerImage(docker, pgImage);
+      console.log(`Pulling ${image} image...`);
+      await pullDockerImage(docker, image);
     }
-    const prunedCount = await pruneContainers(docker, label);
-    if (prunedCount > 0) {
-      console.log(`Pruned ${prunedCount} existing PostgreSQL containers`);
-    }
-    console.log('Creating PostgreSQL container...');
-    const container = await docker.createContainer({
-      Labels: { [label]: 'true' },
-      Image: pgImage,
-      ExposedPorts: { '5432/tcp': {} },
-      HostConfig: {
-        PortBindings: {
-          '5432/tcp': [{ HostPort: '0' }], // 0 to assign a random port
-        },
-      },
-      Env: [
-        `POSTGRES_USER=${pgConfig.PGUSER}`,
-        `POSTGRES_PASSWORD=${pgConfig.PGPASSWORD}`,
-        `POSTGRES_DB=${pgConfig.PGDATABASE}`,
-        `POSTGRES_PORT=5432`,
-      ],
+    console.log(`Creating ${image} container...`);
+    const exposedPorts = ports.reduce(
+      (acc, port) => ({ ...acc, [`${port.container}/tcp`]: {} }),
+      {}
+    );
+    const portBindings: Record<string, { HostPort: string }[]> = {};
+    ports.forEach(port => {
+      portBindings[`${port.container}/tcp`] = [{ HostPort: port.host.toString() }];
     });
-    console.log('Starting PostgreSQL container...');
+    const container = await docker.createContainer({
+      Labels: { [testContainerLabel]: 'true' },
+      Image: image,
+      ExposedPorts: exposedPorts,
+      HostConfig: { PortBindings: portBindings },
+      Env: env,
+    });
+
+    console.log(`Starting ${image} container...`);
     await container.start();
 
-    // Inspect container to get the host port assigned
-    const containerInfo = await container.inspect();
-    pgConfig.PGPORT = containerInfo.NetworkSettings.Ports['5432/tcp'][0].HostPort;
-    console.log(`Postgres container started on port ${pgConfig.PGPORT}`);
+    console.log(`${image} container started on ports ${JSON.stringify(ports)}`);
 
-    Object.assign(globalThis, { __PG_CONTAINER_ID: container.id });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const containerIds = ((globalThis as any).__TEST_DOCKER_CONTAINER_IDS as string[]) ?? [];
+    containerIds.push(container.id);
+    Object.assign(globalThis, { __TEST_DOCKER_CONTAINER_IDS: containerIds });
 
-    for (const entry of Object.entries(pgConfig)) {
-      process.env[entry[0]] = entry[1];
-    }
-
-    // Wait for the database to be ready
-    await waitForPostgres();
+    return { image, containerId: container.id };
   } catch (error) {
     console.error('Error starting PostgreSQL container:', error);
     throw error;
   }
+}
+
+async function findFreePorts(count: number) {
+  const servers = await Promise.all(
+    Array.from({ length: count }, () => {
+      return new Promise<net.Server>((resolve, reject) => {
+        const server = net.createServer();
+        server.listen(0, () => resolve(server)).on('error', reject);
+      });
+    })
+  );
+  const ports = await Promise.all(
+    servers.map(server => {
+      const { port } = server.address() as net.AddressInfo;
+      return new Promise<number>(resolve => server.close(() => resolve(port)));
+    })
+  );
+  return ports;
 }
 
 // Helper function to wait for PostgreSQL to be ready
@@ -109,14 +126,128 @@ async function waitForPostgres(): Promise<void> {
   console.log('Postgres is ready');
 }
 
+async function waitForRedis(): Promise<void> {
+  const redisClient = createClient({
+    url: process.env['REDIS_URL'],
+    name: 'salt-n-pepper-server-tests',
+  });
+  redisClient.on('error', (err: Error) => console.error('Redis Client Error', err));
+  redisClient.once('ready', () => console.log('Connected to Redis successfully!'));
+  while (true) {
+    try {
+      await redisClient.connect();
+      break;
+    } catch (error) {
+      console.error(`Failed to connect to Redis:`, error);
+      await timeout(100);
+    }
+  }
+  await redisClient.disconnect();
+}
+
+async function waitForSNP(): Promise<void> {
+  const snpUrl = process.env['SNP_OBSERVER_URL'];
+  if (!snpUrl) {
+    throw new Error('SNP_OBSERVER_URL is not set');
+  }
+  while (true) {
+    try {
+      const response = await fetch(snpUrl + '/status');
+      if (response.ok) {
+        console.log('SNP is ready');
+        break;
+      } else {
+        console.error(`SNP not ready: ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error(`SNP not ready: ${error}`);
+    }
+    await timeout(100);
+  }
+}
+
 // Jest global setup
 export default async function setup(): Promise<void> {
+  const docker = new Docker();
+  const prunedCount = await pruneContainers(docker, testContainerLabel);
+  if (prunedCount > 0) {
+    console.log(`Pruned ${prunedCount} existing test docker containers`);
+  }
+
+  const [pgHostPort, redisHostPort, snpHostPort] = await findFreePorts(3);
+
+  const startPg = async () => {
+    const pgPort = 5432;
+    const pgContainer = await startContainer({
+      docker,
+      image: 'postgres:17',
+      ports: [{ container: pgPort, host: pgHostPort }],
+      env: [
+        `POSTGRES_USER=${pgConfig.PGUSER}`,
+        `POSTGRES_PASSWORD=${pgConfig.PGPASSWORD}`,
+        `POSTGRES_DB=${pgConfig.PGDATABASE}`,
+        `POSTGRES_PORT=${pgPort}`,
+      ],
+    });
+    pgConfig.PGPORT = pgHostPort.toString();
+    for (const entry of Object.entries(pgConfig)) {
+      process.env[entry[0]] = entry[1];
+    }
+    process.env['_PG_DOCKER_CONTAINER_ID'] = pgContainer.containerId;
+    // Wait for the database to be ready
+    await waitForPostgres();
+  };
+
+  const startRedis = async () => {
+    const redisPort = 6379;
+    const redisContainer = await startContainer({
+      docker,
+      image: 'redis:7',
+      ports: [{ container: redisPort, host: redisHostPort }],
+      env: [],
+    });
+    process.env['REDIS_URL'] = `redis://127.0.0.1:${redisHostPort}`;
+    process.env['_REDIS_DOCKER_CONTAINER_ID'] = redisContainer.containerId;
+    // wait for redis to be ready
+    await waitForRedis();
+  };
+
+  const startServices = await Promise.allSettled([startPg(), startRedis()]);
+  for (const service of startServices) {
+    if (service.status === 'rejected') {
+      throw service.reason;
+    }
+  }
+
+  const startSNP = async () => {
+    const snpObserverPort = 3022;
+    process.env.REDIS_STREAM_KEY_PREFIX = `test_${crypto.randomUUID()}`;
+    console.log(`Using REDIS_STREAM_KEY_PREFIX: ${process.env.REDIS_STREAM_KEY_PREFIX}`);
+    const snpContainer = await startContainer({
+      docker,
+      image: 'hirosystems/salt-n-pepper:1.1.1',
+      ports: [{ container: snpObserverPort, host: snpHostPort }],
+      env: [
+        `OBSERVER_HOST=0.0.0.0`,
+        `OBSERVER_PORT=${snpObserverPort}`,
+        `REDIS_URL=redis://host.docker.internal:${redisHostPort}`,
+        `REDIS_STREAM_KEY_PREFIX=${process.env.REDIS_STREAM_KEY_PREFIX}`,
+        `PGHOST=host.docker.internal`,
+        `PGPORT=${pgHostPort}`,
+        `PGUSER=${pgConfig.PGUSER}`,
+        `PGPASSWORD=${pgConfig.PGPASSWORD}`,
+        `PGDATABASE=${pgConfig.PGDATABASE}`,
+        `PGSCHEMA=test_snp_${crypto.randomUUID()}`,
+      ],
+    });
+    process.env['SNP_OBSERVER_URL'] = `http://127.0.0.1:${snpHostPort}`;
+    process.env['_SNP_DOCKER_CONTAINER_ID'] = snpContainer.containerId;
+    // Wait for snp to be ready
+    await waitForSNP();
+  };
+  await startSNP();
+
   // Setup misc required env vars
   process.env.STACKS_NODE_RPC_HOST = '';
   process.env.STACKS_NODE_RPC_PORT = '1';
-  process.env.CHAINHOOK_NODE_AUTH_TOKEN = '';
-
-  process.env.REDIS_URL = 'redis://127.0.0.1:7379';
-
-  await startPostgresContainer();
 }
